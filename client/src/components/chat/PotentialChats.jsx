9ce3645bb3f40ea909271c661efd3461b69a468f
Manishkujire <minnu0371@gmail.com>
2024-02-18 12:36:04 +0530

    first commit

--- client/src/components/chat/PotentialChats.jsx
+++ client/src/components/chat/PotentialChats.jsx
@@ -1,21 +1,24 @@
 import React, { useContext } from 'react'
 import { ChatContext } from '../../context/chatContext'
+
 export default function PotentialChats() {
-    const {potentialChats}=useContext(ChatContext)
-console.log("potentialChat:",potentialChats)
+    const { potentialChats } = useContext(ChatContext)
 
   return (
     <>
-    <div className="all-users">{
-potentialChats&& potentialChats.map((user,index)=>{
-  return (
-    <div className="single-user" key={index}>{
-      user.name}
-      <span className='user-online'></span></div>
-  )
+      <div className="all-users">{
+        potentialChats && potentialChats.map((user, index) => {
+          return (
+            <div className="single-user" key={index}>{
+              user.name}
+              <span className='user-online'></span></div>
+          )
 
-})
+        })
 
-}</div></>
+      }</div></>
   )
 }
+
+
+
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -5,7 +5,7 @@ import { Alert, Container, Stack } from 'react-bootstrap';
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/chatContext';
 import UserChat from '../components/chat/UserChat';
-import PotentialChats from '../components/chat/PotentialChats'
+import PotentialChats from '../components/chat/PotentialChats';
 export default function Chat() {
   const { user } = useContext(AuthContext)
   const navigate = useNavigate()
@@ -23,7 +23,7 @@ export default function Chat() {
       <Container>
         {userChats?.length < 1 ? null : (<Stack direction='horizontal' gap={4} className='align-items-start'>
           <Stack className='messages-box  flex-grow-0 pe-3' gap={3} >
-     <PotentialChats/>       {
+     <PotentialChats/>     {
  isUserChatsLoading?(<p>Loading...</p>):(userChats?.map((chat,index)=>{
   return(<div key={index}>
     <UserChat chat={chat} user={user}/>
